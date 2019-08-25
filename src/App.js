7e21d152aa6f4692733c22043276f5c1ce821b00
binhtd <binhtd.com@gmail.com>
2019-08-25 12:08:47 +0700

    create-react-app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends Component {
+
+  render(){
+    return (
+      <div className="App container">
+        <h2>MERN Stack</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
